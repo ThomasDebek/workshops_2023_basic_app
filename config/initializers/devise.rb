6e51530bd1de03f62bc4874963eb87e298cbe02d
ThomasDebek <tom.debek@gmail.com>
2023-05-14 12:42:25 +0200

    feat: integeration api with google calendar

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,13 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :google_oauth2, A9n.google_client_id, A9n.google_client_secret, {
+    access_type: "offline",
+    prompt: "consent",
+    select_account: true,
+    scope: 'userinfo.email, calendar'
+  }
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
@@ -310,10 +317,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :google_oauth2,
-                  A9n.google_client_id,
-                  A9n.google_client_secret,
-                  {}
+
 
 
 end
